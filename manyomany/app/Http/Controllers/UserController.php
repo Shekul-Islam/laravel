ae9f6e94284f9880b56e10341f0f00d56e61c8e4
Shekul-Islam <shekul.bd201@gmail.com>
2024-08-29 19:00:32 +0600

    update

--- manyomany/app/Http/Controllers/UserController.php
+++ manyomany/app/Http/Controllers/UserController.php
@@ -4,17 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use App\Models\Role;
 
 class UserController extends Controller
 {
-    public function index (){
+    public function index(){
         $users=User::with('roles')->get();
-
-        // dd($users);
-        // return $users;
         return view('user', compact('users'));
     }
 }
-
-
